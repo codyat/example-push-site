e6080b1cf9f46f5454c884b68df94cc9c6d4d180
Cody Troyer <codyat@users.noreply.github.com>
2020-12-03 19:12:18 -0600

    Update WatchlistDeny.php
    correcting detail

--- wp-content/themes/bb-theme/WatchlistDeny.php
+++ wp-content/themes/bb-theme/WatchlistDeny.php
@@ -1,1 +1,1 @@
-<p>It looks like you have chosen to deny push notifications from volatilitybox.com.<br>In order to use the out Watchlist feature, you must allow push notifications.</p>
+<p>It looks like you have chosen to deny push notifications.<br>In order to use the out Watchlist feature, you must allow push notifications.</p>
